af8628d868712b1d494fa4ff76970f3a2cc2ae29
Alex Marcolina <amarcolina@gmail.com>
2018-08-24 00:05:25 -0700

    Added bounding box example and fixed results

--- Assets/NativeResult/Runtime/Ops/MaximumBounds.cs
+++ Assets/NativeResult/Runtime/Ops/MaximumBounds.cs
@@ -5,11 +5,15 @@ namespace Unity.Collections {
   public struct MaximumBounds : IComutativeOp<Bounds> {
 
     public void Combine(ref Bounds curr, ref Bounds value) {
-      curr.Encapsulate(value);
+      if (curr.extents.x < 0) {
+        curr = value;
+      } else if (value.extents.x >= 0) {
+        curr.Encapsulate(value);
+      }
     }
 
     public void GetIdentity(out Bounds identity) {
-      identity = new Bounds(new Vector3(0, 0, 0), new Vector3(float.MinValue, float.MinValue, float.MinValue));
+      identity = new Bounds(new Vector3(0, 0, 0), new Vector3(-1, -1, -1));
     }
   }
 }
